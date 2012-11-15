b30ab50a69fc2777656495d1de3e0c4235424c16
John Weis <weis.john@gmail.com>
2012-11-15 16:22:24 -0500

    more work for the brand new machine setup

--- new_machine.sh
+++ new_machine.sh
@@ -1,14 +1,20 @@
-
 #!/bin/bash
+
+# make default folders for code
+mkdir $HOME/src && mkdir $HOME/mysrc
 
 # install brew
 ruby -e "$(curl -fsSkL raw.github.com/mxcl/homebrew/go)"
 
-# run the .brew file, this gives us bin 
+# run the .brew file, this gives us git, node, wget, ack
 $HOME/.brew
 
+# install nvm
 
 
 # install z
+cd $HOME/src
+git clone git://github.com/rupa/z.git
 
 # install rvm 
+curl -L https://get.rvm.io | bash -s stable --ruby
