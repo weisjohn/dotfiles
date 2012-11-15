b5a67f7c45aa47961209440ff587fdf27be881d7
John Weis <weis.john@gmail.com>
2012-11-15 16:06:40 -0500

    adding script to handle new machine

---
+++ new_machine.sh
@@ -0,0 +1,14 @@
+
+#!/bin/bash
+
+# install brew
+ruby -e "$(curl -fsSkL raw.github.com/mxcl/homebrew/go)"
+
+# run the .brew file, this gives us bin 
+$HOME/.brew
+
+
+
+# install z
+
+# install rvm 
