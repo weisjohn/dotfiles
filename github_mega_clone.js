5e59ba9f000869eb377138c153c86a9b87e27593
John Weis <weis.john@gmail.com>
2012-11-15 21:34:03 -0500

    proof of concept achieved

--- github_mega_clone.js
+++ github_mega_clone.js
@@ -1,27 +1,25 @@
 var request = require('request'),
-	u = require('underscore')
-	exec = require('child_process').exec,
+    u = require('underscore')
+    exec = require('child_process').exec,
     child,
-	repos;
+    repos;
 
 request('https://api.github.com/users/weisjohn/repos', function (error, response, body) {
-  if (!error && response.statusCode == 200) {
+    if (!error && response.statusCode == 200) {
     
-  	// grab all the repos from github
-    repos = JSON.parse(body);
+        // grab all the repos from github
+        repos = JSON.parse(body);
 
     // retrive the ssh locations, process them one by one
-    u.pluck(repos, 'ssh_url').each(function(ssh_url) { 
-
-		child = exec('git clone ' + ssh_url,
-		  function (error, stdout, stderr) {
-		    console.log('stdout: ' + stdout);
-		    console.error('stderr: ' + stderr);
-		    if (error !== null) {
-		      console.log('exec error: ' + error);
-		    }
-		});
-
+    u.pluck(r, 'ssh_url').each(function(ssh_url) { 
+      child = exec('git clone ' + ssh_url,
+        function (error, stdout, stderr) {
+            console.log('stdout: ' + stdout);
+            console.error('stderr: ' + stderr);
+            if (error !== null) {
+                console.log('exec error: ' + error);
+            }
+        });
     });
 
   }
