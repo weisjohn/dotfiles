b9900f0004779343455f18b1779142f9c8afda34
John Weis <weis.john@gmail.com>
2012-11-15 22:10:48 -0500

    organization

--- new_machine.sh
+++ new_machine.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 # make default folders for code
-mkdir $HOME/src && mkdir $HOME/mysrc
+mkdir -p $HOME/src && mkdir -p $HOME/mysrc
+
+##################################################################
+# languages and utilities       								 #
+##################################################################
 
 # install brew
 ruby -e "$(curl -fsSkL raw.github.com/mxcl/homebrew/go)"
@@ -9,13 +13,16 @@ ruby -e "$(curl -fsSkL raw.github.com/mxcl/homebrew/go)"
 # run the .brew file, this gives us git, wget, ack
 $HOME/.brew
 
+# install pygments for docco
+sudo easy_install pygments
+
 # install nvm , node latest
 git clone git://github.com/creationix/nvm.git $HOME/nvm
 . $HOME/nvm/nvm.sh
 nvm install 0.8.14
 
 # install global node utilties 
-npm install -g coffee-script 
+npm install -g coffee-script docco
 
 # install z
 cd $HOME/src
@@ -24,11 +31,24 @@ git clone git://github.com/rupa/z.git
 # install rvm 
 curl -L https://get.rvm.io | bash -s stable --ruby
 
+##################################################################
+# code						       								 #
+##################################################################
+
 # download all personal github repos into mysrc folder
 cp ./github_mega_clone.js $HOME/mysrc
 cd $HOME/mysrc
 npm install request underscore
 node github_mega_clone.js
 rm -rvf $HOME/mysrc/node_modules github_mega_clone.js
+
+
+##################################################################
+# applications						       						 #
+##################################################################
+
+cd $HOME/Downloads
+
+wget 
 
 echo "please run the bootstrap.sh file now"
