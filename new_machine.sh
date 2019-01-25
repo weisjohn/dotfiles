5dc759fc8adfcbb01f88db8c7a924a4c9a03bf4d
John Weis <weis.john@gmail.com>
2019-01-25 13:10:17 -0500

    new: altcaps via npm

--- new_machine.sh
+++ new_machine.sh
@@ -46,7 +46,7 @@ nvm install $NODE_VERSION
 nvm use $NODE_VERSION
 
 # install global node utilties
-npm install -g bunyan kamino nodemon slugify-cli fkill-cli
+npm install -g bunyan kamino nodemon slugify-cli fkill-cli altcaps
 
 # install gvm
 bash < <(curl -sSL https://raw.githubusercontent.com/markeissler/gvm2/master/binscripts/gvm-installer)
